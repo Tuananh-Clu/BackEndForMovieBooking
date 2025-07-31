dede8920344c50986d867b5eecbf8cfb54ab3fbe
Tuananh-Clu <yianh798@gmail.com>
2025-07-31 14:31:19 +0700

    fix Program.cs

--- MovieTicketWebApi/Program.cs
+++ MovieTicketWebApi/Program.cs
@@ -12,7 +12,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 builder.Services.AddHttpClient();
@@ -60,10 +59,6 @@ builder.Services.AddSingleton<IMongoClient>(sp =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 if (app.Environment.IsDevelopment())
 {
     app.MapSwagger();
