1091b1c6824364f86cd0d487b329b5aed182bc17
Tuananh-Clu <yianh798@gmail.com>
2025-08-01 20:42:36 +0700

    fix pic enclosure

--- MovieTicketWebApi/Model/Article/Enclosure.cs
+++ MovieTicketWebApi/Model/Article/Enclosure.cs
@@ -1,15 +1,20 @@
 ﻿using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
 
 namespace MovieTicketWebApi.Model.Article
 {
     public class Enclosure
     {
         [BsonElement("link")]
+        [JsonPropertyName("link")]
         public string Link { get; set; }
 
         [BsonElement("type")]
+        [JsonPropertyName("type")]
         public string Type { get; set; }
-        [BsonElement("lenght")]
+
+        [BsonElement("length")] 
+        [JsonPropertyName("length")]
         public int Length { get; set; }
     }
 }
