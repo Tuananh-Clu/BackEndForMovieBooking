0085155dd69f6c9dbcb1ca41e7d4791e4fb45cb9
Tuananh-Clu <yianh798@gmail.com>
2025-08-08 20:54:48 +0700

    sac

--- MovieTicketWebApi/Model/Ticket/TicketInformation.cs
+++ MovieTicketWebApi/Model/Ticket/TicketInformation.cs
@@ -14,7 +14,8 @@ namespace MovieTicketWebApi.Model.Ticket
         public int Quantity { get; set; } = 0;
         public string Image { get; set; } = "";
         public string SeatType { get; set; } = "";
-        public string location { get; set; } = "";
+        [JsonPropertyName("Location")]
+        public string Location { get; set; } = "";
         public string City { get; set; } = "";
         public string IsSelected { get; set; } = "true";
     }
