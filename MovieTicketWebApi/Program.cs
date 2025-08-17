bd2c75f4fda690988e0eb5b60cd58f863010f931
Tuananh-Clu <yianh798@gmail.com>
2025-08-17 21:12:04 +0700

    Futa

--- MovieTicketWebApi/Program.cs
+++ MovieTicketWebApi/Program.cs
@@ -80,7 +80,6 @@ app.UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "MovieTicket API v1");
 });
-app.UseCors("AllowFrontend");
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
