3d21074f0ff9ab204fd6e69ddf6af76060a391a7
Tuananh-Clu <yianh798@gmail.com>
2025-08-25 14:43:28 +0700

    fu

--- MovieTicketWebApi/Service/Voucher/VoucherService.cs
+++ MovieTicketWebApi/Service/Voucher/VoucherService.cs
@@ -59,7 +59,7 @@ namespace MovieTicketWebApi.Service.Voucher
         {
             var filter=Builders<VoucherDb>.Filter.Eq(a=>a.Code, code);
             var data=await _voucherCollection.Find(filter).FirstOrDefaultAsync();
-            if (!data.PhamViApDung.Trim().ToLower().Contains(theaterName.Trim().ToLower())) {
+            if (!string.Equals(data.PhamViApDung.Trim().ToLower(),theaterName.Trim().ToLower())) {
                 return ("Không thể áp dụng voucher cho rạp bạn đang chọn");
             }
             string Price;
